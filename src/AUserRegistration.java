bd5ee102e412e8b11480c594d5ca8fa0a9380374
Alevtyna777 <119439779+Alevtyna777@users.noreply.github.com>
2023-01-31 00:54:53 +0200

    Homework # 17.

--- src/AUserRegistration.java
+++ src/AUserRegistration.java
@@ -20,6 +20,10 @@ public class AUserRegistration {
     int theNumberOFStepsTakenPerDay;
     private int age;
 
+    public AUserRegistration(String michael, String s, String email, String s1, int i, String s2, int i1) {
+    }
+
+
     public void AUserRegistrationVariableParameters(String lastName, int weight, int pressure, int theNumberOFStepsTakenPerDay) {
         this.lastName = lastName;
         this.weight = weight;
@@ -67,17 +71,38 @@ public class AUserRegistration {
         this.lastName =lastName;
 
 }
-    public void setWeight(String weight) {
+    public void setWeight(int weight) {
         this.weight = weight;
 
     }
-    public void setTheNumberOFStepsTakenPerDay(String heNumberOFStepsTakenPerDay) {
+    public void setTheNumberOFStepsTakenPerDay(int theNumberOFStepsTakenPerDay) {
         this.theNumberOFStepsTakenPerDay = theNumberOFStepsTakenPerDay;
     }
 
 
        public void printAUserRegistrationInformation(){
         System.out.println("Display current parameters specifed by a user");
+    }
+    @Override
+    public String toString() {
+return "AUserRegistrationInformation:\n" +
+        "firstName" + this.firstName + "\n" +
+       "birthdayDate" + this.birthdayDate + "\n" +
+       "email" + this.email  + "\n" +
+        "phoneNumbe"+ this.phoneNumber  + "\n" +
+       "lastName " + this.lastName + "\n" +
+        " weight" +  this.weight+ "\n" +
+        " pressure" +  this.pressure  + "\n" +
+        "theNumberOFStepsTakenPerDay  " +  this.theNumberOFStepsTakenPerDay  + "\n" +
+        " age " + this.age + "\n" + "\n" ;
+
+
+    }
+
+    public void setEmail(String s) {
+    }
+
+    public void setPressure(int i, int i1) {
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,15 @@
 
 public class Main {
     public static void main(String[] args) {
+        AUserRegistration michael = new AUserRegistration("Michael", "18,11,1991", " Rosynskiy91@gmail.com", "+15049367531", 80, "75, 110", 15000);
+        michael.printAUserRegistrationInformation();
+        michael.setEmail(" Rosynskiy91@gmail.com");
+        michael.setPressure(75,100);
 
+
+        AUserRegistration louis= new AUserRegistration("Louis", "01,12,1962", " Armstrong62@gmail.com", "+15049257531", 90, "93, 130", 11000);
+        louis.printAUserRegistrationInformation();
+        AUserRegistration jimmy = new AUserRegistration(" Jimmy ","17,11,1960", " Hendrix60@gmail.com", "+15049367831", 85, "87, 120", 23000 );
+        jimmy.printAUserRegistrationInformation();
     }
 }
